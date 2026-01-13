b5069d6489687c7869a484ad48869a575d156c02
nasrul1221 <fst20071221@gmail.com>
2026-01-13 22:37:37 +0200

    feat: Add CORS middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 require('dotenv').config()
 
 // Routes
@@ -11,6 +12,9 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+// CORS
+app.use(cors())
 
 app.use('/tasks', taskRouter)
 app.use('/categories', categoryRouter)
